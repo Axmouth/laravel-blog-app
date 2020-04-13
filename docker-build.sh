9d922c35bfb9ff39f718f7e0d0ba668cb1377b5f
Axmouth <aggronfg@yahoo.com>
2020-04-13 21:03:49 +0300

    test

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 docker-compose build
 docker-compose up -d --remove-orphans
-COMPOSE_INTERACTIVE_NO_CLI=1 docker-compose exec laravel-blog-app php artisan migrate
+COMPOSE_INTERACTIVE_NO_CLI=1 docker-compose -T exec laravel-blog-app php artisan migrate
 
