8cb4ad66e4ea8ff29522e16d6a186499399a20f8
Axmouth <aggronfg@yahoo.com>
2020-04-13 07:02:03 +0300

    Made the database run inside docker

--- docker-build.sh
+++ docker-build.sh
@@ -2,3 +2,4 @@
 docker build -f Dockerfile -t axmouth/laravel-blog-server .
 #docker build -f server/apache/Dockerfile -t axmouth/laravel-blog-apache .
 docker-compose up -d --remove-orphans
+docker-compose exec laravel-blog-app php artisan migrate
