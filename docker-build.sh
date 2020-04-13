b6996fe486d134ce772673570a1435fcd8bb3251
Axmouth <aggronfg@yahoo.com>
2020-04-13 18:54:27 +0300

    Removed chown step from build script

--- docker-build.sh
+++ docker-build.sh
@@ -3,5 +3,4 @@ docker build -f Dockerfile -t axmouth/laravel-blog-server .
 #docker build -f server/apache/Dockerfile -t axmouth/laravel-blog-apache .
 docker-compose up -d --remove-orphans
 docker-compose exec laravel-blog-app php artisan migrate
-docker-compose exec laravel-blog-app bash ownstoragefiles.sh
 
