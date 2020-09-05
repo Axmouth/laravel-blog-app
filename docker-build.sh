e280460e3505fb5a40827004d33a4f9a1870e689
Axmouth <aggronfg@yahoo.com>
2020-09-05 19:23:45 +0300

    Made migrations run in prod

--- docker-build.sh
+++ docker-build.sh
@@ -10,7 +10,7 @@ docker-compose exec -T laravel-blog-app-backend php ./artisan view:clear
 docker-compose exec -T laravel-blog-app-backend php ./artisan optimize
 docker-compose exec -T laravel-blog-app-backend rm -rf public/storage
 docker-compose exec -T laravel-blog-app-backend php ./artisan storage:link
-docker-compose exec -T laravel-blog-app-backend php ./artisan migrate
+docker-compose exec -T laravel-blog-app-backend php ./artisan migrate --force
 
 docker-compose exec -T laravel-blog-app-backend mkdir -p /var/www/storage/app/public/profile_images
 docker-compose exec -T laravel-blog-app-backend mkdir -p /var/www/storage/app/public/cover_images
